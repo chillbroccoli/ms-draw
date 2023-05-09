a9deb97f635e6254afd8fe54d048771f99ce8aa6
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-09 21:37:19 +0200

    extract header to component

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
   import CanvasBoard from "$lib/components/canvas_board.svelte";
+  import Header from "$lib/components/header.svelte";
   import Toolbar from "$lib/components/toolbar.svelte";
 </script>
 
 <main class="main">
-  <header>
-    <div class="title-bar title-bar-container">
-      <div class="title-bar-text">untitled - Paint</div>
-      <div class="title-bar-controls">
-        <button aria-label="Minimize" />
-        <button aria-label="Maximize" />
-        <button aria-label="Close" />
-      </div>
-    </div>
-    <div class="actions-buttons">
-      <button>File</button>
-    </div>
-  </header>
-
+  <Header />
   <div class="grid">
     <Toolbar />
     <div>
@@ -31,22 +19,6 @@
     height: 100vh;
     overflow: hidden;
   }
-
-  .title-bar-container {
-    padding: 0.75rem;
-  }
-
-  .actions-buttons {
-    display: flex;
-    background-color: #c0c0c0;
-  }
-
-  .actions-buttons > button {
-    border: none !important;
-    outline: none !important;
-    box-shadow: none !important;
-  }
-
   .grid {
     height: 100%;
     display: grid;
