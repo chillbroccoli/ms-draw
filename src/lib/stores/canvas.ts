fa2c4899de13717e33fa6ce5894bd790d7712f35
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-09 21:38:54 +0200

    change store properties names

--- src/lib/stores/canvas.ts
+++ src/lib/stores/canvas.ts
@@ -2,14 +2,12 @@ import { writable } from "svelte/store";
 
 type CanvasStore = {
   color: string;
-  paths: { x: number; y: number }[][];
-  imageData: ImageData | null;
-  fontThickness: number;
+  snapshots: ImageData[];
+  lineThickness: number;
 };
 
 export const canvasStore = writable<CanvasStore>({
-  paths: [],
   color: "black",
-  imageData: null,
-  fontThickness: 2,
+  snapshots: [],
+  lineThickness: 2,
 });
