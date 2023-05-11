dc27c855cc4348bbdf972bc3bfc5793af959e8b2
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-11 14:54:08 +0200

    prettier format

--- src/lib/constants/colors.ts
+++ src/lib/constants/colors.ts
@@ -1,31 +1,31 @@
 export default [
-  "#000000", // black
-  "#FFFFFF", // white
-  "#C0C0C0", // silver
-  "#808080", // gray
-  "#FF0000", // red
-  "#800000", // maroon
-  "#FFFF00", // yellow
-  "#808000", // olive
-  "#00FF00", // lime
-  "#008000", // green
-  "#00FFFF", // aqua
-  "#008080", // teal
-  "#0000FF", // blue
-  "#000080", // navy
-  "#FF00FF", // fuchsia
-  "#800080", // purple
-  "#FFA07A", // light salmon
-  "#FF6347", // tomato
-  "#FFD700", // gold
-  "#FF8C00", // dark orange
-  "#FF4500", // orange red
-  "#00CED1", // dark turquoise
-  "#40E0D0", // turquoise
-  "#00BFFF", // deep sky blue
-  "#1E90FF", // dodger blue
-  "#ADD8E6", // light blue
-  "#AFEEEE", // pale turquoise
-  "#7FFFD4", // aquamarine
-  "#DDA0DD", // plum
+	"#000000", // black
+	"#FFFFFF", // white
+	"#C0C0C0", // silver
+	"#808080", // gray
+	"#FF0000", // red
+	"#800000", // maroon
+	"#FFFF00", // yellow
+	"#808000", // olive
+	"#00FF00", // lime
+	"#008000", // green
+	"#00FFFF", // aqua
+	"#008080", // teal
+	"#0000FF", // blue
+	"#000080", // navy
+	"#FF00FF", // fuchsia
+	"#800080", // purple
+	"#FFA07A", // light salmon
+	"#FF6347", // tomato
+	"#FFD700", // gold
+	"#FF8C00", // dark orange
+	"#FF4500", // orange red
+	"#00CED1", // dark turquoise
+	"#40E0D0", // turquoise
+	"#00BFFF", // deep sky blue
+	"#1E90FF", // dodger blue
+	"#ADD8E6", // light blue
+	"#AFEEEE", // pale turquoise
+	"#7FFFD4", // aquamarine
+	"#DDA0DD" // plum
 ];
