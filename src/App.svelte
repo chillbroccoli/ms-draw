45dc32482080b4e27db9bdc7436464e20b1d2685
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-11 14:52:36 +0200

    prettier format

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
-  import CanvasBoard from "$lib/components/canvas_board.svelte";
-  import Header from "$lib/components/header.svelte";
-  import Toolbar from "$lib/components/toolbar.svelte";
+	import CanvasBoard from "$lib/components/canvas_board.svelte";
+	import Header from "$lib/components/header.svelte";
+	import Toolbar from "$lib/components/toolbar.svelte";
 </script>
 
 <main class="main">
-  <Header />
-  <div class="grid">
-    <Toolbar />
-    <div>
-      <CanvasBoard />
-    </div>
-  </div>
+	<Header />
+	<div class="grid">
+		<Toolbar />
+		<CanvasBoard />
+	</div>
 </main>
 
 <style>
-  .main {
-    height: 100vh;
-    overflow: hidden;
-  }
-  .grid {
-    height: 100%;
-    display: grid;
-    grid-template-columns: 2fr 14fr;
-    grid-template-rows: 100%;
-  }
+	.main {
+		height: 100vh;
+		overflow: hidden;
+	}
+	.grid {
+		height: 100%;
+		display: grid;
+		grid-template-columns: 2fr 14fr;
+		grid-template-rows: 100%;
+	}
 </style>
